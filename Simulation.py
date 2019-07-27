ed35f8346fd9fa11bf53c01fdfd4d87edbad8d99
Ruben G. Tsui <37323290+ruben-tsui@users.noreply.github.com>
2019-07-27 12:36:27 +0800

    Add files via upload

---
+++ Simulation.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from scipy.stats import norm, poisson
+from numpy import sqrt, exp, log
+import math
+
+def merton_jump_diffusion_simulate(params):
+    '''
+    Merton Jump Diffusion simulation
+    S0 = 100    # spot price
+    K  = 100    # strike price
+    r  = 0.05   # risk free rate
+    q  = 0.02   # dividend yield
+    σ  = 0.2    # volatility
+    t  = 1      # maturity
+    λ  = 3      # jump rate
+    γ  = 0.1    # jump mean
+    δ  = 0.2    # jump volatility
+    Δt = 0.001  # discretization step
+    s  = 123456 # seed for pseudorandom number generation 
+    '''
+    S0 = params['S0']
+    K  = params['K']
+    r  = params['r']
+    q  = params['q']
+    σ  = params['σ']
+    t  = params['t']
+    λ  = params['λ']
+    γ  = params['γ']
+    δ  = params['δ']
+    Δt = params['Δt']
+    s  = params['seed']
+    np.random.seed(np.random.randint(0,65535))
+
+    #random.normal(loc=0.0, scale=1.0, size=None)
+    #print(f"μ={μ}, σ={σ}, Δt={Δt}, N={N}\n")
+
+    M = np.int(1/Δt)
+    S = np.zeros(M + 1)
+    S[0] = S0
+
+    k = exp(γ + δ*δ/2) - 1
+    µ = (r - q - λ*k)
+
+    for t in range(1, M + 1):
+        Z = norm.rvs(0, 1)
+        N = poisson.rvs(λ * Δt)
+        X = sum([norm.rvs(γ, δ*δ) for i in range(0, N)])
+        S[t] = S[t - 1] + (μ - σ*σ/2) * Δt + σ * sqrt(Δt) * Z + X
+    return exp(S)
+
+def heston_stochastic_volatility_simulate(params):
+    '''
+    Heston stochastic volatility model
+    S0 = 100    # spot price
+    K  = 100    # strike price
+    r  = 0.05   # risk risk-free interest rate for the stock
+    q  = 0.02   # dividend yield
+    κ  =        # mean reversion speed for the variance
+    θ  =        # the mean reversion level for the variance
+    σ  = 0.2    # volatility of variance
+    ν0 = 0.2    # initial level of the variance
+    ρ  =        # correlation between the two Brownian motions W1 and W2
+
+    t  = 1      # maturity
+    Δt = 0.001  # discretization step
+    s  = 123456 # seed for pseudorandom number generation 
+    '''
+    S0 = params['S0']
+    K  = params['K']
+    r  = params['r']
+    q  = params['q']
+    κ  = params['κ']
+    θ  = params['θ']
+    σ  = params['σ']
+    v0 = 0.35 # params['v0']
+    ρ  = params['ρ']
+
+    #t  = params['t']
+    Δt = params['Δt']
+    s  = params['seed']
+    #np.random.seed(s)
+
+    np.random.seed(np.random.randint(0,65535))
+
+    M = np.int(1/Δt)
+    S = np.zeros(M + 1)
+    S[0] = S0
+    v = np.zeros(M + 1)
+    v[0] = v0
+
+    for t in range(1, M + 1):
+
+        # Generate two independent random variables $Z_1$ and $Z_2$
+        Z1, Z2 = norm.rvs(0, 1), norm.rvs(0, 1)
+
+        # define Z_V = Z_1 and Z_S = ρZ V + √(1 − ρ^2) * Z_2
+        ZV = Z1
+        ZS = ρ * ZV + sqrt(1-ρ*ρ) * Z2
+
+        # Stochastic variance
+        v[t] = ( sqrt(v[t-1]) + σ*sqrt(Δt)*ZV/2 )**2 + κ*(θ - v[t-1])*Δt - σ*σ*Δt/4
+
+        # log-stock price
+        S[t] = S[t - 1] + (r - q - v[t-1]/2) * Δt + sqrt(v[t-1] * Δt) * ZS
+
+    return exp(S), v
+
+
+
+def binomial_randomwalk_multiplicative_simulate(params):
+    '''
+    Multiplicative binomial random walk simulation
+    S0 = 100   # initial price
+    u  = 1.1   # "up" factor
+    d  = 0.9   # "down" factor, currently set to the multiplicative inverse of u
+    p  = 0.5   # probability of "up"
+    T  = 30    # time-step size
+    N  = 50000 # sample size (no. of simulations)
+    '''
+    S0 = params['S0']
+    p  = params['p']
+    u  = params['u']
+    d  = 1/u
+    T  = params['T']
+    N  = params['N']
+    P  = params['P']
+    s  = params['seed']
+    np.random.seed(s)
+    # Simulating N paths with T time steps
+    S = np.zeros((T + 1, N))
+    S[0] = S0
+    for t in range(1, T + 1):
+        z = np.random.rand(N)  # pseudorandom numbers
+        S[t] = S[t - 1] * ( (z<=p)*u + (z>p)*d )
+          # vectorized operation per time step over all paths
+    return S
+
+def returnSampleDescriptiveStatistics(S, K, decimalPlaces=2):
+    '''
+    Returns mean, variance, skewness, kurtosis, etc.
+    '''
+    samp_mean = f"{S[-1, :].mean():.2f}"
+    samp_var = f"{S[-1, :].var():.2f}"
+    samp_skew = f"{skew(S[-1, :]):.2f}"
+    samp_kurt = f"{kurtosis(S[-1, :]):.2f}"
+    
+    #samp_X_minus_K = f"{S[-1, :].mean():.2f}"
+    #samp_K_minus_X = 
+
+
+#def random_walk
+
+def generate_path(S0, r, sigma, T, M):
+	''' 
+	Source: Hilpisch 2017, p. 236
+	
+	Function to simulate a geometric Brownian motion.
+	Parameters
+	==========
+		S0: float
+		initial index level
+		r: float
+		constant risk-less short rate
+		sigma: float
+		instantaneous volatility
+		T: float
+		date of maturity (in year fractions)
+		M: int
+		number of time intervals
+	Returns
+	=======
+		path: pandas DataFrame object simulated path
+	'''
+	# length of time interval
+
+	dt = float(T) / M
+	# random numbers
+	np.random.seed(100000)
+	rn = np.random.standard_normal(M + 1)
+	rn[0] = 0 # to keep the initial value
+	# simulation of path
+	path = S0 * np.exp(np.cumsum((r - 0.5 * sigma ** 2) * dt + sigma * sqrt(dt) * rn))
+	# setting initial value
+	path = pd.DataFrame(path, columns=['index'])
+	return path
+
+
---
+++ main.py
@@ -0,0 +1,425 @@
+# -*- coding: utf-8 -*-
+
+import time, datetime, os
+#os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+
+import sys
+#import design
+#from PyQt5.QtCore import Qt
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QHBoxLayout, QSlider, QLineEdit, QLabel, QTableWidgetItem, QSizePolicy
+from matplotlib.widgets import Slider, Button, RadioButtons
+
+# Libraries for drawing figures
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.ticker import FuncFormatter
+# Ensure using PyQt5 backend
+import matplotlib as mpl
+mpl.use('QT5Agg')
+import matplotlib.pyplot as plt
+#plt.style.use('ggplot')
+#plt.style.use('seaborn')
+#plt.tight_layout(pad = 1.25)
+from matplotlib.ticker import FuncFormatter
+
+# Libraries for stochastic simulation
+import numpy as np
+from numpy import log, exp, sqrt
+from scipy.stats import skew, kurtosis
+from math import ceil, floor
+from scipy.special import binom
+from scipy.stats import norm
+#import bs_call_base as bsm
+
+from Simulation import merton_jump_diffusion_simulate as mjd, heston_stochastic_volatility_simulate as hsv
+
+class Window(QtWidgets.QDialog):
+    def __init__(self):
+        QtWidgets.QDialog.__init__(self)
+        # load the .ui file created in Qt Creator directly
+        print(f"screen width = {screen_width}")
+        if screen_width >= 1920:
+            Interface = uic.loadUi('mod4.ui', self)
+        else:
+            Interface = uic.loadUi('mod4.ui', self)
+
+        # other useful info
+        # default values for all parameters
+        ## Common parameters
+        self.N = int(self.comboBox_SimSize.currentText()) # simulation size
+        self.Δt = 0.002
+        ## Merton
+        self.S0 = 100
+        self.K = 100
+        self.r = 0.02
+        self.q = 0.01
+        self.σ = 0.20
+        self.t = 100
+        self.λ = 3
+        self.γ = 0.15
+        self.δ = 0.10
+        self.S = []
+        self.SN = []
+        self.path_Merton = 0  # this is the path being selected currently
+        self.returns = []
+        ## Heston
+        self.S0_Heston = 100
+        self.K_Heston  = 100
+        self.r_Heston  = 0.02
+        self.q_Heston  = 0.01
+        self.σ_Heston  = 0.20
+        self.t_Heston  = 100
+        self.κ_Heston  = 0.20
+        self.θ_Heston  = 0.20
+        self.ρ_Heston  = 0.25
+        self.v0_Heston = 0.35
+        self.S_Heston = []
+        self.SN_Heston = []
+        self.path_Heston = 0  # this is the path being selected currently
+        self.returns_Heston = []
+        self.volatility_Heston = []
+        #self.seed = 12345
+        self.seed = np.random.randint(0,65535)
+        params = {'S0': log(self.S0), 'K': log(self.K), 'r': self.r, 'q': self.q, 'σ': self.σ, 't': self.t, 'λ': self.λ, 'γ': self.γ, 'δ': self.δ,  'Δt': self.Δt, 'seed': self.seed}
+        self.params = params
+
+        # Merton model
+        self.slider_K.valueChanged.connect(self.on_change_K)
+        self.slider_r.valueChanged.connect(self.on_change_r)
+        self.slider_q.valueChanged.connect(self.on_change_q)
+        self.slider_sigma.valueChanged.connect(self.on_change_sigma)
+        self.slider_t.valueChanged.connect(self.on_change_t)
+        self.slider_lambda.valueChanged.connect(self.on_change_lambda)
+        self.slider_gamma.valueChanged.connect(self.on_change_gamma)
+        self.slider_delta.valueChanged.connect(self.on_change_delta)
+
+        self.button_Simulate.clicked.connect(self.simulate_Merton)
+
+        self.slider_t1.valueChanged.connect(self.on_change_t)
+
+        # Heston model
+        self.slider_K_Heston.valueChanged.connect(self.on_change_K_Heston)
+        self.slider_r_Heston.valueChanged.connect(self.on_change_r_Heston)
+        self.slider_q_Heston.valueChanged.connect(self.on_change_q_Heston)
+        self.slider_sigma_Heston.valueChanged.connect(self.on_change_sigma_Heston)
+        self.slider_kappa_Heston.valueChanged.connect(self.on_change_kappa_Heston)
+        self.slider_theta_Heston.valueChanged.connect(self.on_change_theta_Heston)
+        self.slider_v0_Heston.valueChanged.connect(self.on_change_v0_Heston)
+        self.slider_rho_Heston.valueChanged.connect(self.on_change_rho_Heston)
+
+        self.button_Simulate_Heston.clicked.connect(self.simulate_Heston)
+        self.slider_t1_Heston.valueChanged.connect(self.on_change_t1_Heston)
+
+        # pick a simulated path
+        self.button_PickSim_Heston.clicked.connect(self.PickSim_Heston)
+        self.button_PickSim_Merton.clicked.connect(self.PickSim_Merton)
+
+        # initialize parameter values - Merton
+        self.label_S0.setText(f"S0 = {self.S0}")
+        self.label_K.setText(f"K = {self.K}")
+        self.label_r.setText(f"r = {self.r}")
+        self.label_q.setText(f"q = {self.q}")
+        self.label_sigma.setText(f"σ = {self.σ}")
+        self.label_lambda.setText(f"λ = {self.λ}")
+        self.label_gamma.setText(f"γ = {self.γ}")
+        self.label_delta.setText(f"δ = {self.δ}")
+
+        # initialize parameter values - Heston
+        self.label_S0_Heston.setText(f"S0 = {self.S0_Heston}")
+        self.label_K_Heston.setText(f"K = {self.K_Heston}")
+        self.label_r_Heston.setText(f"r = {self.r_Heston}")
+        self.label_q_Heston.setText(f"q = {self.q_Heston}")
+        self.label_kappa_Heston.setText(f"κ = {self.κ_Heston}")
+        self.label_theta_Heston.setText(f"θ = {self.θ_Heston}")
+        self.label_sigma_Heston.setText(f"σ = {self.σ_Heston}")
+        self.label_v0_Heston.setText(f"σ = {self.v0_Heston}")
+        self.label_rho_Heston.setText(f"ρ = {self.ρ_Heston}")
+        
+        # Make sure window size is set
+        self.setGeometry(0, 0, 1320, 768)
+
+    # Heston model
+    def on_change_t1_Heston(self, value):
+        self.t_Heston = value
+        self.label_t1_Heston.setText(f"t = {str(self.t_Heston)}")
+        self.showGraph_Heston()
+    def on_change_K_Heston(self, value):
+        self.K_Heston = value
+        self.label_K_Heston.setText(f"K = {str(self.K_Heston)}")
+    def on_change_sigma_Heston(self, value):
+        self.σ_Heston = value/100
+        self.label_sigma_Heston.setText(f"σ = {str(self.σ_Heston)}")
+    def on_change_r_Heston(self, value):
+        self.r_Heston = value/100
+        self.label_r_Heston.setText(f"r = {str(self.r_Heston)}")
+    def on_change_q_Heston(self, value):
+        self.q_Heston = value/100
+        self.label_q_Heston.setText(f"q = {str(self.q)}")
+    def on_change_kappa_Heston(self, value):
+        self.κ_Heston = value/100
+        self.label_kappa_Heston.setText(f"κ = {str(self.κ_Heston)}")
+    def on_change_theta_Heston(self, value):
+        self.θ_Heston = value/100
+        self.label_theta_Heston.setText(f"θ = {str(self.θ_Heston)}")
+    def on_change_v0_Heston(self, value):
+        self.v0_Heston = value/100
+        self.label_v0_Heston.setText(f"v0 = {str(self.v0_Heston)}")
+    def on_change_rho_Heston(self, value):
+        self.ρ_Heston = value/100
+        self.label_rho_Heston.setText(f"ρ = {str(self.ρ_Heston)}")
+
+
+
+    # Merton model
+    def on_change_t(self, value):
+        self.t = value
+        self.label_t1.setText(f"t = {str(self.t)}")
+        self.showGraph()
+    def on_change_K(self, value):
+        self.K = value
+        self.label_K.setText(f"K = {str(self.K)}")
+        self.showGraph()
+    def on_change_sigma(self, value):
+        self.σ = value/100
+        self.label_sigma.setText(f"σ = {str(self.σ)}")
+    def on_change_r(self, value):
+        self.r = value/100
+        self.label_r.setText(f"r = {str(self.r)}")
+    def on_change_q(self, value):
+        self.q = value/100
+        self.label_q.setText(f"q = {str(self.q)}")
+    # λγδ
+    def on_change_lambda(self, value):
+        self.λ = value
+        self.label_lambda.setText(f"λ = {str(self.λ)}")
+    def on_change_gamma(self, value):
+        self.γ = value/100 - 0.20
+        self.label_gamma.setText(f"γ = {str(self.γ)}")
+    def on_change_delta(self, value):
+        self.δ = value/100
+        self.label_delta.setText(f"λ = {str(self.δ)}")
+
+    def on_click_ShowSimulation(self):
+        print("Simulation button clicked")
+        self.showGraph()
+
+    def simulate_Merton(self):
+        #S0 = 100
+        #K = 100; r = 0.025; q = 0.005; σ = 0.2; T = 1.0
+        S0 = self.S0
+        K  = self.K
+        r  = self.r
+        q  = self.q
+        σ  = self.σ
+        t  = self.t
+        λ  = self.λ
+        γ  = self.γ
+        δ  = self.δ
+        Δt = self.Δt
+        params = {'S0': log(S0), 'K': log(K), 'r': r, 'q': q, 'σ': σ, 't': t, 'λ': λ, 'γ': γ, 'δ': δ,  'Δt': Δt, 'seed': self.seed}
+        #print(f"params = {params}")
+        self.params = params
+        self.SN = []
+        for i in range(self.N):
+            self.SN.append(mjd(self.params))
+        self.SN = np.array(self.SN)
+        self.showGraph()
+
+    def simulate_Heston(self):
+        #S0 = 100
+        #K = 100; r = 0.025; q = 0.005; σ = 0.2; T = 1.0
+        S0 = self.S0_Heston
+        K  = self.K_Heston
+        r  = self.r_Heston
+        q  = self.q_Heston
+        σ  = self.σ_Heston
+        t  = self.t_Heston
+        κ  = self.κ_Heston
+        θ  = self.θ_Heston
+        ρ  = self.ρ_Heston
+        v0 = self.v0_Heston
+        Δt = self.Δt
+        params = {'S0': log(S0), 'K': log(K), 'r': r, 'q': q, 'σ': σ, 'κ': κ, 'θ': θ, 'ρ': ρ, 'v0': v0, 'Δt': Δt, 'seed': self.seed}
+        #print(f"params = {params}")
+        self.params_Heston = params
+        self.SN_Heston = []
+        self.volatility_Heston = []
+        for i in range(self.N):
+            log_stock_prices, volatility = hsv(self.params_Heston)
+            self.SN_Heston.append(log_stock_prices)
+            self.volatility_Heston.append(volatility)
+        self.SN_Heston = np.array(self.SN_Heston)
+        self.volatility_Heston = np.array(self.volatility_Heston)
+        self.showGraph_Heston()
+
+
+    def showGraph(self):
+
+        mpl = self.oMplCanvas.canvas
+        [s1, s2, s3, s4] = mpl.axes
+
+        S0 = self.S0
+        K  = self.K
+        r  = self.r
+        q  = self.q
+        σ  = self.σ
+        t  = self.t
+        λ  = self.λ
+        γ  = self.γ
+        δ  = self.δ
+        Δt = self.Δt
+        self.S = self.SN[self.path_Merton]
+
+        s4.set_visible(False)
+
+        s1.clear()
+        s1.set_title('Metron Jump Diffusion $S_t$', fontsize=14, color='brown')
+        s1.yaxis.set_tick_params(labelright=False, labelleft=True)
+        s1.grid(True)
+        s1.plot(self.S, 'b', lw=0.5, label='present value')
+
+        s2.clear()
+        s2.set_title(r'$ln{\frac{S_{t+Δt}}{S_t}}$', fontsize=14, color='brown')
+        s2.axvline(x=self.t, color="blue", alpha=0.5, linewidth=1)
+        s2.grid(True)
+        M = self.S[t]
+        returns = [ log(self.S[t+1]) - log(self.S[t]) for t in range(0, self.S.size-1)]
+        s2.plot(returns, 'r', lw=0.5, label='return')
+        #s2.axvline(x=slope, color="g", alpha=0.75, linewidth=2)
+        #s2.plot(x, z_put, 'b-.', lw=1.5, label='payoff')
+
+        ###########################################
+        # Plot s3: return distribution histogram 
+        ###########################################
+        returns = []
+        
+        for i in range(self.N):
+            ret = [ log(self.SN[i,t+1]) - log(self.SN[i,t]) for t in range(0, self.S.size-1)]
+            returns.append(ret)
+        self.returns = returns
+        #returns = [ log(self.S[t+1]) - log(self.S[t]) for t in range(0, self.S.size-1)]
+        #returns_dist_at_t = [ log(self.SN[:, t+1]) - log(self.SN[:, t]) for t in range(0, self.SN.size-1)]
+        returns = [ log(self.SN[:,self.t]) - log(self.SN[:,self.t-1])] 
+        s3.clear()
+        s3.hist(returns, density=True, bins=20, orientation='vertical', alpha=0.40, color='green')
+        s3.set_title(r"Return Distribution", fontsize=14, color='brown')
+        s3.yaxis.set_ticks_position("right")
+        s3.xaxis.set_major_formatter(FuncFormatter('{0:.1%}'.format))
+
+        s3.grid(True)
+        s3.tick_params(axis = 'both', which = 'major', labelsize = 6)
+        s3.set_xlabel('Return', fontsize=8, color='brown')
+
+        mpl.fig.set_visible(True)
+        mpl.draw()
+
+
+    def PickSim_Merton(self):
+        self.path_Merton = np.random.randint(0, self.N)
+        self.showGraph()        
+        return None
+
+    def PickSim_Heston(self):
+        '''
+        Out of the many simulated paths (self.N). choose one and display it with showGraph_Heston()
+        numpy.random.randint(low, high=None, size=None, dtype='l')
+            Return random integers from low (inclusive) to high (exclusive).
+        '''
+        self.path_Heston = np.random.randint(0, self.N)
+        self.showGraph_Heston()        
+        return None
+
+    def showGraph_Heston(self):
+
+        mpl = self.oMplCanvas_Heston.canvas
+        [s1, s2, s3, s4] = mpl.axes
+
+        S0 = self.S0_Heston
+        K  = self.K_Heston
+        r  = self.r_Heston
+        q  = self.q_Heston
+        σ  = self.σ_Heston
+        t  = self.t_Heston
+        κ  = self.κ_Heston
+        θ  = self.θ_Heston
+        ρ  = self.ρ_Heston
+        v0 = self.v0_Heston
+        Δt = self.Δt
+        self.S_Heston = self.SN_Heston[self.path_Heston]
+
+        s1.clear()
+        s1.set_title('Heston Stochastic Volatility Model $S_t$', fontsize=11, color='brown')
+        s1.yaxis.set_tick_params(labelright=False, labelleft=True)
+        s1.grid(True)
+        s1.plot(self.S_Heston, 'b', lw=0.5, label='present value')
+
+        s2.clear()
+        s2.set_title(r'$ln{\frac{S_{t+Δt}}{S_t}}$', fontsize=12, color='brown')
+        s2.axvline(x=self.t_Heston, color="blue", alpha=0.5, linewidth=2)
+        s2.yaxis.set_major_formatter(FuncFormatter('{0:.1%}'.format))
+        #s2.axvline(x=self.t, color="blue", alpha=0.5, linewidth=1)
+        s2.grid(True)
+        M = self.S_Heston[t]
+        # these are the returns for the given path
+        returns = [ log(self.S_Heston[t+1]) - log(self.S_Heston[t]) for t in range(0, self.S_Heston.size-1)]
+        s2.plot(returns, 'r', lw=0.5, label='return')
+        #s2.axvline(x=slope, color="g", alpha=0.75, linewidth=2)
+        #s2.plot(x, z_put, 'b-.', lw=1.5, label='payoff')
+
+        ###########################################
+        # Plot s3: return distribution histogram 
+        ###########################################
+        returns = []
+        
+        for i in range(self.N):
+            ret = [ log(self.SN_Heston[i,t+1]) - log(self.SN_Heston[i,t]) for t in range(0, self.S_Heston.size-1)]
+            returns.append(ret)
+        self.returns_Heston = returns
+        #returns = [ log(self.S_Heston[t+1]) - log(self.S_Heston[t]) for t in range(0, self.S_Heston.size-1)]
+        #returns_dist_at_t = [ log(self.SN[:, t+1]) - log(self.SN[:, t]) for t in range(0, self.SN.size-1)]
+        # we want the simulated return distribution at time a particular time t in range(0, self.S_Heston.size-1)
+        returns = [ log(self.SN_Heston[:,self.t_Heston]) - log(self.SN_Heston[:,self.t_Heston-1])] 
+
+        s3.clear()
+        s3.hist(returns, density=False, bins=20, orientation='vertical', alpha=0.40, color='green')
+        s3.set_title(r"Return Distribution", fontsize=11, color='brown')
+        s3.yaxis.set_ticks_position("right")
+        s3.xaxis.set_major_formatter(FuncFormatter('{0:.1%}'.format))
+
+        s3.grid(True)
+        s3.tick_params(axis = 'both', which = 'major', labelsize = 6)
+        s3.set_xlabel('Return', fontsize=8, color='brown')
+
+        ###############################################################
+        # Plot s4: stochastic volatility distribution histogram 
+        ###############################################################
+        volatility_at_t = self.volatility_Heston[:,self.t_Heston]
+        s4.clear()
+        s4.hist(volatility_at_t, density=True, stacked=True, bins=25, orientation='vertical', alpha=0.70, color='orange')
+        s4.set_title(r"Stochastic Volatility Distribution", fontsize=14, color='brown')
+        s4.xaxis.set_major_formatter(FuncFormatter('{0:.1%}'.format))
+        s4.grid(True)
+        s4.tick_params(axis = 'both', which = 'major', labelsize = 6)
+        s4.set_xlabel('volatility', fontsize=8, color='brown')
+
+        ### draw all graphs
+        mpl.fig.set_visible(True)
+        mpl.draw()
+
+
+
+# This creates the GUI window:
+if __name__ == '__main__':
+
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    ### load appropriately sized UI based on screen resolution detected
+    screen = app.primaryScreen()
+    screen_width=screen.size().width()
+    window = Window()
+    w = window
+    window.show()
+    sys.exit(app.exec_())
+
